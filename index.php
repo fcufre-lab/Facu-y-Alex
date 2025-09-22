52334baf9f40f5a5e49aae4cc036bf408142f91a
fcufre-lab <fcufre@ideo.edu.ar>
2025-09-22 14:30:17 -0300

    Cambios

--- index.php
+++ index.php
@@ -9,8 +9,17 @@
 
    <h1>Te recomendamos.com</h1>
     
+    <h1>Te recomendamos.com</h1>
     
+    <h3>Esta pagina esta basada sobre videojuegos, peliculas y series. Y también como fue su impacto en nuestra cultura e industria</h3>
 
+    <img src="Proyecto final.png">
+
+    <text>Los videojuegos, peliculas y series son diferentes industrias del entretenimiento que llegaron a tener un gran impacto en nuestra cultura de hoy en dia desde el entretenimiento y la estimulación de la creatividad hasta nuestra conducta y en nuestra salud mental y fisica</text>
+
+    <text>Los videojuegos por excelencia, es la industria que mas impacto tuvo en en entretenimiento, esto se debe a su mayores ingresos en comparación a las peliculas y series combinados. Ademas cabe aclarar que también surgieron otros modelos de negocios gracias a los videojuegos, incluyendo que promueve el trabajo en equipo en ciertos juegos y el pensamiento logico.</text>
+
+    <h2>LE DAS A ACEPTAR</h2>
 
 </body>
 </html>
